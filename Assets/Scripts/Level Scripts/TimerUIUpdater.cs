39ff171f6c75dc17f655436c60b79ad747226e64
Adil Bhayani <abha808@aucklanduni.ac.nz>
2017-12-14 20:52:40 +1300

    Timer updates within the timerButton

---
+++ Assets/Scripts/Level Scripts/TimerUIUpdater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUIUpdater : MonoBehaviour {
+
+    public float startTime;
+    public Text timerText;
+    private int minutes;
+    private int seconds;
+	
+	// Update is called once per frame
+	void Update () {
+        startTime -= Time.deltaTime;
+        minutes = (int)startTime / 60;
+        Debug.Log("minutes is: " + minutes);
+        seconds = (int)startTime - minutes * 60;
+        timerText.text = string.Format("Timer: {0}:{1}", minutes.ToString("D2"),seconds.ToString("D2"));
+    }
+}

654dc5724ab0837d6912a450a37b0c8041c17cc6
Adil Bhayani <abha808@aucklanduni.ac.nz>
2018-01-15 19:25:16 +1300

    Color changes when resistor is clicked

---
+++ Assets/Scripts/Component Scripts/Resistor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resistor : MonoBehaviour {
+	public GameObject resistor;
+	private SpriteRenderer spirit;
+	private bool selected = false;
+
+	// Use this for initialization
+	void Start () {
+		spirit = resistor.GetComponent<SpriteRenderer> ();
+		spirit.color = new Color (1f, 1f, 1f, 1f);
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		if (selected) {
+			spirit.color = Color.white;
+			selected = false;
+		} else {
+			spirit.color = Color.yellow;
+			selected = true;
+		}
+	}
+}
